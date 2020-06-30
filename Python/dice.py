575d98c672ee53311a11f39f56190b8e99a72050
Golakoti G V P Sai Ram <53946433+pavan581@users.noreply.github.com>
2020-06-30 15:03:19 +0530

    Update dice.py

--- Python/dice.py
+++ Python/dice.py
@@ -6,12 +6,12 @@ a = random.randrange(1,7)
 print(a)
 
 x = {
-    1:Image.open(r"D:\pavan\Android\XML files\dice XML\dice_1.jpg"),
-    2:Image.open(r"D:\pavan\Android\XML files\dice XML\dice_2.jpg"),
-    3:Image.open(r"D:\pavan\Android\XML files\dice XML\dice_3.jpg"),
-    4:Image.open(r"D:\pavan\Android\XML files\dice XML\dice_4.jpg"),
-    5:Image.open(r"D:\pavan\Android\XML files\dice XML\dice_5.jpg"),
-    6:Image.open(r"D:\pavan\Android\XML files\dice XML\dice_6.jpg")
+    1:Image.open(r"dice_1 image address"),
+    2:Image.open(r"dice_2 image address"),
+    3:Image.open(r"dice_3 image address"),
+    4:Image.open(r"dice_4 image address"),
+    5:Image.open(r"dice_5 image address"),
+    6:Image.open(r"dice_6 image address")
     }
 
 im = x.get(a)
