9589099be1a2157c6d4b2197347f5c9f154ff618
pavan <sairam.g581@gmail.com>
2020-06-30 14:30:14 +0530

    Modified to show dice image

--- Python/dice.py
+++ Python/dice.py
@@ -1,5 +1,21 @@
+from PIL import Image
 import random
 
 a = random.randrange(1,6)
 
 print(a)
+
+if(a == 1):
+    im = Image.open(r"D:\pavan\Android\XML files\dice XML\dice_1.jpg")
+elif(a == 2):
+    im = Image.open(r"D:\pavan\Android\XML files\dice XML\dice_2.jpg")
+elif(a == 3):
+    im = Image.open(r"D:\pavan\Android\XML files\dice XML\dice_3.jpg")
+elif(a == 4):
+    im = Image.open(r"D:\pavan\Android\XML files\dice XML\dice_4.jpg")
+elif(a == 5):
+    im = Image.open(r"D:\pavan\Android\XML files\dice XML\dice_5.jpg")
+elif(a == 6):
+    im = Image.open(r"D:\pavan\Android\XML files\dice XML\dice_6.jpg")
+    
+im.show()
