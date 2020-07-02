657e01b82e0494be66d686c52f5573c9c3c75be3
pavan <sairam.g581@gmail.com>
2020-07-02 21:06:49 +0530

    Exception handling

--- Python/wikipedia.py
+++ Python/wikipedia.py
@@ -2,14 +2,19 @@ import wikipedia
 import os
 from gtts import gTTS
 
-search_word = input("Wikipedia> ")
 language = 'en'
-try:
-    text = wikipedia.summary(search_word, sentences=1)
-except:
-    text = "An unexpected error occured while searching."
 
-finally:
-    myobj = gTTS(text=text)
-    myobj.save("Testing.mp3")
-    os.system("Testing.mp3")
+while True:
+    search_word = input("Wikipedia> ")
+
+
+    try:
+        text = wikipedia.summary(search_word, sentences=1)
+    except:
+        text = "An unexpected error occured while searching."
+
+    finally:
+        myobj = gTTS(text=text, lang=language, slow=False)
+        myobj.save("Testing.mp3")
+        os.system("Testing.mp3")
+        
