e65ff74aa2dfab9811b3d1444ef1e22ea0fc7c81
pavan <sairam.g581@gmail.com>
2020-07-03 13:38:27 +0530

    Takes voice as input

--- Python/wikipedia.py
+++ Python/wikipedia.py
@@ -1,20 +1,39 @@
-import wikipedia
 import os
+import time
+import pyttsx3
+import wikipedia
 from gtts import gTTS
+from mutagen.mp3 import MP3
+import speech_recognition as sr
+
+r = sr.Recognizer()
 
 language = 'en'
 
+def SpeakText(command):
+    engine = pyttsx3.init()
+    engine.say(command)
+    engine.runAndWait()
+
 while True:
-    search_word = input("Wikipedia> ")
-
-
     try:
-        text = wikipedia.summary(search_word, sentences=1)
+        with sr.Microphone() as source:
+            r.adjust_for_ambient_noise(source)
+            print("say something...")
+            audio = r.listen(source)
+            MyText = r.recognize_google(audio)
+            print(MyText)
+            result = wikipedia.summary(MyText, sentences=1)
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+    
     except:
-        text = "An unexpected error occured while searching."
+        result = "Unknown error occured while listening or searching..."
 
     finally:
-        myobj = gTTS(text=text, lang=language, slow=False)
+        myobj = gTTS(text=result, lang=language, slow=False)
         myobj.save("Testing.mp3")
         os.system("Testing.mp3")
-        
+        audio = int(MP3("Testing.mp3").info.length)
+        print(audio)
+        time.sleep(int(MP3("Testing.mp3").info.length)+2)
