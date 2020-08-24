f9e197ac57ae39ace6eda907913d8d12e48cde6c
Golakoti G V P Sai Ram <53946433+pavan581@users.noreply.github.com>
2020-08-24 21:34:28 +0530

    Added comments for better understanding.

--- Python/passwordencrypt.py
+++ Python/passwordencrypt.py
@@ -6,6 +6,7 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import base64
 
+#read contents from json file
 def _read(account,ID):
     try:
         fp = open("passwords.json","r")
@@ -18,6 +19,7 @@ def _read(account,ID):
         fp.close()
         return obj.get(account).get(ID) 
             
+#write contents to json file
 def _write(account,ID,Pass):
     try:
         fp = open("passwords.json","r")
@@ -32,13 +34,17 @@ def _write(account,ID,Pass):
 
     try:
         fp = open("passwords.json","w")
+        
         if account not in obj.keys():
             obj[account] = {ID:Pass}
+            
         elif account in obj.keys():
+            
             if ID not in obj[account]:
                 obj[account][ID] = Pass
                 print("Password added.")
                 print(f"{account}\n{ID}:{password}")
+            
             elif ID in obj[account]:
                 print("Password found.")
                 print("Changing password...")
@@ -51,7 +57,7 @@ def _write(account,ID,Pass):
     finally:
         fp.close()
 
-
+#create an empty json file
 def __init__():
 
     try:
@@ -62,7 +68,9 @@ def __init__():
         fp.close()
 
 while True:
+    #Mode
     mode = input("Enter mode['encrypt','e','decrypt','d','change','c']: ").lower()
+    
     if mode[0] in ['e','c']:
         account = input("Enter the account name: ")
         ID = input("Enter account ID: ")
@@ -77,7 +85,7 @@ while True:
         continue
 
     key = input("Enter the key: ").encode()
-    salt = b'SALT'
+    salt = b'SALT' #can be anything else as 'SALT' as mentioned here But should be same always while encrypting and decrypting.
     kdf = kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
         length=32,
@@ -90,20 +98,23 @@ while True:
     f = Fernet(_key)
 
     if mode[0] in ['e','c']:
-        Pass = f.encrypt(password)
-        print(f"Encrypted.\n{account}\n{ID}: {Pass}")
-        _write(account,ID,Pass.decode())
+        Pass = f.encrypt(password) #Encrypt password with encrypt() method
+        print(f"Encrypted.\n{account}\n{ID}: {Pass}") #print encrypted passwrod with account name and ID.
+        _write(account,ID,Pass.decode()) #save to json file.
 
     elif mode[0] == 'd':
         try:
-            password = _read(account,ID)
+            password = _read(account,ID) #read password from json file.
             if password is None:
                 print("Password not found.")
                 continue
-            Pass = f.decrypt(password.encode())
-            print(f"Decrypted.\n{account}\n{ID}: {Pass.decode()}")
+                
+            Pass = f.decrypt(password.encode()) #Decrypt password with decrypt() method.
+            print(f"Decrypted.\n{account}\n{ID}: {Pass.decode()}") #print decrypted password with account name and ID.
+        
         except AttributeError:
-            print("Can't find account.")
+            print("Can't find account.") #if account name is not in the file
+            
         except:
-            print("Sorry! key didn't match.\nEnter correct key.")
+            print("Sorry! key didn't match.\nEnter correct key.") #if key didn't match
 
